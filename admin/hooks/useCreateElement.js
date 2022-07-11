c30c54227d9b4e94e408d6680cea41feae321f3b
전상욱 <sanguk@jeonsang-ug-ui-MacBookPro.local>
2022-07-11 15:08:33 +0900

    backup

--- admin/hooks/useCreateElement.js
+++ admin/hooks/useCreateElement.js
@@ -1,5 +1,5 @@
-export default function useCreateElement (tagName = 'div', attribute = {}, children = '') {
-  if (typeof(tagName) !== 'string' || typeof(attribute) !== 'object' || typeof(children) !== 'string') return null;
+export default function useCreateElement(tagName = 'div', attribute = {}, children = '') {
+  if (typeof (tagName) !== 'string' || typeof (attribute) !== 'object' || typeof (children) !== 'string') return null;
   let tag = document.createElement(tagName);
   for (let key in attribute) {
     tag.setAttribute(key, attribute[key]);
